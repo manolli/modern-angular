e6f50554bc4dc4fa8020b0b895fc33302159d2b0
Lucas Manolli de Oliveira Cristo <lucas.manolli.sfs@gmail.com.br>
2024-11-21 00:18:12 -0300

    ajuste para adicionar o valor de subtotal do carrinho ao lado do icone de cart  (testes), tem que atualizar  a forma como está sendo feito o if

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -3,7 +3,11 @@ import { Component, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTooltipModule } from '@angular/material/tooltip';
 import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Observable } from 'rxjs';
+import { Signal } from '@angular/core';
 
 import { CartService } from './../../cart/cart.service';
 
@@ -12,10 +16,21 @@ import { CartService } from './../../cart/cart.service';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
   standalone: true,
-  imports: [MatToolbarModule, MatButtonModule, RouterLink, MatIconModule, AsyncPipe]
+  imports: [MatToolbarModule, MatButtonModule, RouterLink, MatIconModule, AsyncPipe,MatTooltipModule,CommonModule],
 })
 export class HeaderComponent {
 
   private cartService = inject(CartService);
   cartCount = this.cartService.cartCount;
+  cartTotal: Signal<number>;
+
+  constructor() {
+    // Assuming you have a service that provides the cart total
+    this.cartTotal = this.cartService.cartTotal;
+  }
+
+  getCartTotalValue(): number {
+    return this.cartTotal();
+  }
+
 }
